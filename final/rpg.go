0f3d404fa9cd3a1ea48a8d9ead238165282f2ba4
tjwever <timjwever@gmail.com>
2022-10-27 09:01:52 -0400

    added functionality to js and go

--- final/rpg.go
+++ final/rpg.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
 
 /////////////////////////////////////////////////////////////////////////////////////
 // CHARACTER
@@ -14,11 +19,12 @@ type Character struct {
 	charClass             string
 	weapon                string
 	newStartingAttributes DefaultAttributes
+	backpack          	  []string
 }
 
 // FAUX CONSTRUCTOR FOR STRUCT DATA TYPE  //  FOR CHARACTER
 func NewCharacter(_name, _gender, _race, _charClass, _weapon string) *Character {
-	return &Character{_name, _gender, _race, _charClass, _weapon, *NewStartingAttributes()}
+	return &Character{_name, _gender, _race, _charClass, _weapon, *NewStartingAttributes(), *backPack(_charClass, _weapon)}
 }
 
 // FUNCTIONS OR METHODS THAT BELONG TO THE CHARACTER DATA TYPE
@@ -56,6 +62,33 @@ func (c *Character) greeting() {
 	fmt.Println("Hello, my name is " + c.name + ". I am a " + c.race + " " + c.charClass)
 }
 
+func (c *Character) attack() {
+	rand.Seed(time.Now().UnixNano())
+	randomDmg := rand.Float64()*c.newStartingAttributes.baseMeleeDmg
+	roll := rand.Intn(20) + 1
+
+	if roll == 20 {
+		randomDmg = c.newStartingAttributes.baseMeleeDmg * 2
+		fmt.Println(c.name, "rolled a natural 20!! CRITIAL HIT!!!" )
+		fmt.Println(randomDmg, "points of damage to", c.name + "'s target!!")
+	} else if roll == 1 {
+		// you're either doing 2 point of damage, or 0 points...rough...
+		if randomDmg > 5 {
+			randomDmg = math.Ceil(rand.Float64()* (5 / 2))
+		} else {
+			randomDmg = math.Floor(rand.Float64()* (randomDmg / 2))
+		}
+		fmt.Println("Oof..", c.name, "rolled a 1..." )
+		fmt.Println(randomDmg, "points of damage to", c.name + "'s target.")
+	} else {
+		fmt.Println(c.name, "rolled a", roll)
+		fmt.Println(c.name, "dealt", randomDmg, "'s of damange")
+
+	}
+
+	c.expPoints()
+}
+
 func (c *Character) useSkill() {
 	skill := c.newStartingAttributes.skillSlot
 	if skill > 0 {
@@ -73,6 +106,37 @@ func (c *Character) dmg() {
 	fmt.Println(c.name, "took damage!", "Health:", c.newStartingAttributes.life)
 }
 
+// Since we can't use 'this.name' or anything that belongs to Character, we have to 
+// think about the way we create this differently.  We also can't bind Character to this
+// function to make it a method and call it in our 'constructor'.  So, we'll have to pass
+// in some values that will take in the parameters that we use for our constructor
+// When charClass is called, it'll turn into whatever we pass in from the constructor.
+// Example: charClass = 'rogue' and weapon = 'dagger'
+func backPack(charClass, weapon string) *[]string {
+	pack := []string{}
+	if charClass == "rogue" {
+		for i := 0; i < 5; i++ {
+			pack = append(pack, "health potion")
+		}
+		pack = append(pack, weapon)
+		pack = append(pack, "leather tunic")
+		pack = append(pack, "leather cap")
+		pack = append(pack, "leather leggings")
+	} else {
+		for i := 0; i < 2; i++ {
+			pack = append(pack, "health potion")
+		}
+		for i := 0; i < 3; i++ {
+			pack = append(pack, "mana potion")
+		}
+		pack = append(pack, weapon)
+		pack = append(pack, "cloth robe")
+		pack = append(pack, "cloth cap")
+		pack = append(pack, "cloth leggings")
+	}
+	return &pack
+}
+
 func (c *Character) stats() {
 	fmt.Println("('=============================')")
 	fmt.Println("STATS FOR:", c.name)
@@ -80,7 +144,7 @@ func (c *Character) stats() {
 	fmt.Println("RACE:", c.race)
 	fmt.Println("LEVEL:", c.newStartingAttributes.level)
 	fmt.Println("EXP:", c.newStartingAttributes.exp)
-	fmt.Println("SKILL SLOTS:", &c.newStartingAttributes.skillSlot)
+	fmt.Println("SKILL SLOTS:", c.newStartingAttributes.skillSlot)
 	fmt.Println("SPECIAL:", *c.applySkill())
 	fmt.Println("BASE DMG:", c.newStartingAttributes.baseMeleeDmg)
 	fmt.Println("('=============================')")
@@ -158,23 +222,17 @@ func (t *Tavern) drink(character *Character) {
 	if t.size == "small" {
 		character.newStartingAttributes.life = character.newStartingAttributes.life * 0.25 + character.newStartingAttributes.life
 		fmt.Println(character.name, "takes a drink at", t.name, "and replenishes their health by 25%")
-		} else if t.size == "medium" {
-			character.newStartingAttributes.life = character.newStartingAttributes.life * 0.50 + character.newStartingAttributes.life
-			fmt.Println(character.name, "takes a drink at", t.name, "and replenishes their health by 50%")
-		}
+	} else if t.size == "medium" {
+		character.newStartingAttributes.life = character.newStartingAttributes.life * 0.50 + character.newStartingAttributes.life
+		fmt.Println(character.name, "takes a drink at", t.name, "and replenishes their health by 50%")
+	}
 }
 
 func (t *Tavern) sleep(character *Character) {
 	if t.hasInn == true {
-		fmt.Println("*********sleep method block start*********")
-		fmt.Println(character)
-		fmt.Println(character.name, "goes to sleep at", t.name, "Inn and is fully rested!")
 		character.newStartingAttributes.life = 100
 		character.newStartingAttributes.skillSlot = 2
-		// fmt.Println(character.newStartingAttributes.life)
-		// fmt.Println(character.newStartingAttributes.skillSlot)
-		fmt.Println(character)
-		fmt.Println("*********sleep method block end*********")
+		fmt.Println(character.name, "goes to sleep at", t.name, "Inn and is fully rested!")
 		} else {
 			fmt.Println(character.name, "is attempting to go to sleep at", t.name, "Inn, but there is no Inn here...")
 		}
@@ -190,8 +248,8 @@ func (t *Tavern) sleep(character *Character) {
 ///////////////////////////////////////////////////////////////////////////////
 func main() {
 	theRogue := *NewCharacter("Zap Branigan", "male", "human", "rogue", "dagger")
-	theTavern := *NewTavern("The BullFrog Creek", "small", true, true)
-	// theRogue.greeting()
+	// theTavern := *NewTavern("The BullFrog Creek", "small", true, true)
+	theRogue.greeting()
 	// theRogue.home()
 	// theRogue.stats()
 	// theRogue.stats()
@@ -201,27 +259,26 @@ func main() {
 	// theRogue.useSkill()
 	// theRogue.useSkill()
 	// fmt.Println("after using skill slot", theRogue.newStartingAttributes.skillSlot)
-	fmt.Println(theRogue.name, "Health:", theRogue.newStartingAttributes.life)
-	fmt.Println(theRogue.name, "Skill Slot:", theRogue.newStartingAttributes.skillSlot)
-	theRogue.useSkill()
-	theRogue.dmg()
-	fmt.Println("before resting skill slot is now", theRogue.newStartingAttributes.skillSlot)
-	fmt.Println("before resting skill slot is now", theRogue.newStartingAttributes.life)
-	theTavern.drink(&theRogue)
-	fmt.Println("after resting skill slot is now", theRogue.newStartingAttributes.skillSlot)
-	fmt.Println("after resting skill slot is now", theRogue.newStartingAttributes.life)
+	// fmt.Println(theRogue.name, "Health:", theRogue.newStartingAttributes.life)
+	// fmt.Println(theRogue.name, "Skill Slot:", theRogue.newStartingAttributes.skillSlot)
+	// theRogue.useSkill()
+	// theRogue.dmg()
+	// fmt.Println("before resting skill slot is now", theRogue.newStartingAttributes.skillSlot)
+	// fmt.Println("before resting skill slot is now", theRogue.newStartingAttributes.life)
+	// theTavern.drink(&theRogue)
+	// fmt.Println("after resting skill slot is now", theRogue.newStartingAttributes.skillSlot)
+	// fmt.Println("after resting skill slot is now", theRogue.newStartingAttributes.life)
 
+	// fmt.Println(theRogue.backpack)
 	
 	// theRogue.stats()
-
+	
 	// theWizard := *NewCharacter("Henry Plopper", "male", "human", "wizard", "staff")
 	// theWizard.stats()
 	// theWizard.greeting()
 	// theWizard.home()
 	// theWizard.useSkill()
-
+	// fmt.Println(theWizard.backpack)
+	
 }
 
-// Something to add.  Want to demo how to deal with lists/arrays.  Was thinking about implementing a storage cache, or stash.
-// Maybe every character can start with a stash with a few items inside, potions, armor and such.  Stash may end up being it's own
-// class in the future, but we can use a simple property on the Character class for now.
--- final/rpg.js
+++ final/rpg.js
@@ -12,12 +12,40 @@ class Character {
         this.lvl = 1
         this.baseMeleeDmg = 10
 
+        this.backpack = []
+
+        // For these examples of if statements (backpack logic and signature skill assignment), there 
+        // small enough to leave in the constructor, but it would be probably be smarter to put them 
+        // in their own methods and just call them in the constructor.
+        // Especially if they get any bigger, or more complex.
+        if(this.charClass === "Rogue") {
+            for(let i = 0; i < 5; i++) {
+                this.backpack.push('health potion')
+            }
+            this.backpack.push(this.weapon)
+            this.backpack.push('leather helm')
+            this.backpack.push('leather tunic')
+            this.backpack.push('leather pants')
+
+        } else if(this.charClass === "Wizard") {
+            for(let i = 0; i < 2; i++) {
+                this.backpack.push('health potion')
+            }
+            for(let i = 0; i < 3; i++) {
+                this.backpack.push('mana potion')
+            }
+            this.backpack.push(this.weapon)
+            this.backpack.push('cloth robe')
+            this.backpack.push('cloth pants')
+        }
+
         this.signatureSkill;
         if(this.charClass === 'Rogue') {
             this.signatureSkill = 'Backstab'
         } else if(this.charClass === 'Wizard') {
             this.signatureSkill = 'FireBall'
         }
+        
     }
 
     expPoints() {
@@ -50,6 +78,9 @@ class Character {
         }
     }
 
+    // Something to implement later:  Going to have to pass in
+    // 'monster' parameters for the attack.  That way when we call
+    // attack(), we can pass in the instantiated Monster and 
     attack() {
         let randomDmg = Math.floor(Math.random() * this.baseMeleeDmg) + 1
         let roll = Math.floor(Math.random() * 20) + 1
@@ -64,7 +95,7 @@ class Character {
             } else {
                 randomDmg = Math.floor(randomDmg / 2)
             }
-            console.log(`${this.name} rolled a natural 1...`)
+            console.log(`${this.name} rolled a 1...`)
             console.log(`${randomDmg} points of damage to ${this.name}'s target`)
         } else {
             console.log(`${this.name} rolled a ${roll}.`)
@@ -141,6 +172,55 @@ class Tavern {
             console.log(`The ${character.charClass} is attempting to go to sleep but, there's no Inn at this Tavern...`)
         }
     }
+
+    takeQuest() {
+        
+    }
+
+    completeQuest() {
+
+    }
+    
+}
+
+class Monster {
+    constructor(_name, _monsterType, _gender, _weapon, _isElite) {
+        this.name = _name
+        this.monsterType = _monsterType
+        this.gender = _gender
+        this.weapon = _weapon
+        this.isElite = _isElite
+
+        // this stuff is determined by the level of the instantiated character
+        this.life
+        this.lvl
+        this.baseMeleeDmg
+        
+    }
+
+    attack() {
+        let randomDmg = Math.floor(Math.random() * this.baseMeleeDmg) + 1
+        let roll = Math.floor(Math.random() * 20) + 1
+
+        if(roll === 20) {
+            randomDmg = this.baseMeleeDmg * 2
+            console.log(`A ${this.name} rolled a natural 20!!  CRITICAL HIT!!!`)
+            console.log(`${randomDmg} points of damage to ${this.name}'s target!!`)
+        } else if(roll === 1) {
+            if(randomDmg > 5) {
+                randomDmg = Math.floor(5 / 2)
+            } else {
+                randomDmg = Math.floor(randomDmg / 2)
+            }
+            console.log(`${this.name} rolled a 1...`)
+            console.log(`${randomDmg} points of damage to ${this.name}'s target`)
+        } else {
+            console.log(`${this.name} rolled a ${roll}.`)
+            console.log(`${this.name} dealt ${randomDmg} points of damage to the target.`)
+        }
+
+    }
+    
 }
 
 // Main program
@@ -148,18 +228,18 @@ let theRogue = new Character('Zap Branigan', 'male', 'human', 'Rogue', 'dagger')
 let theWizard = new Character('Gandolf', 'male', 'human', 'Wizard', 'wand');
 let homeTavern = new Tavern('The BullFrog Creek', 'small', false, true)
 
-console.log('The Rogues life is at', theRogue.life)
-theRogue.life = 50
-console.log('The Rogue is damaged, and has a life bar of', theRogue.life)
+// console.log('The Rogues life is at', theRogue.life)
+// theRogue.life = 50
+// console.log('The Rogue is damaged, and has a life bar of', theRogue.life)
 
-homeTavern.drink(theRogue)
-console.log(`The ${theRogue.charClass}'s life is at ${theRogue.life}`)
-homeTavern.sleep(theRogue)
+// homeTavern.drink(theRogue)
+// console.log(`The ${theRogue.charClass}'s life is at ${theRogue.life}`)
+// homeTavern.sleep(theRogue)
 
-console.log('The Rogues life is at', theRogue.life)
+// console.log('The Rogues life is at', theRogue.life)
 
-console.log(`---`)
-theRogue.stats()
+// console.log(`---`)
+// theRogue.stats()
 
 // theRogue.greeting()
 // theWizard.greeting()
@@ -172,3 +252,12 @@ theRogue.stats()
 
 // theRogue.useSkill()
 // theRogue.stats()
+
+// console.log(theRogue.backpack)
+// console.log(theWizard.backpack)
+
+theRogue.attack()
+theRogue.attack()
+theRogue.attack()
+theRogue.attack()
+theRogue.attack()
